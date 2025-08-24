91d4b796d0f84d54c157589391b9f9584673be81
matheusmoyaoliveira <matheus.dev.moya@hotmail.com>
2025-08-23 22:28:06 -0300

    começando no HackerRank

---
+++ arithmetic-operators.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    soma = a + b
+    sub = a - b
+    multi = a * b
+    
+    print(soma)
+    print(sub)
+    print(multi)
